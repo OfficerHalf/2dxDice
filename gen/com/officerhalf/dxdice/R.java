5a5117f8a6ad5d3a82df939cc8bd83f7a98b0815
Nathan Smith <natsmith@dhcp-10-15-1-84.hsv.redhat.com>
2013-08-02 13:28:33 -0500

    Added save/recall functionality. Modified layout slightly.

--- gen/com/officerhalf/dxdice/R.java
+++ gen/com/officerhalf/dxdice/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080003;
+        public static final int action_settings=0x7f080008;
+        public static final int button1=0x7f080004;
         public static final int dicePicker=0x7f080000;
-        public static final int roll_output=0x7f080005;
+        public static final int recall_button=0x7f080007;
+        public static final int roll_output=0x7f080002;
+        public static final int save_button=0x7f080006;
         public static final int sidesPicker=0x7f080001;
-        public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080004;
+        public static final int textView1=0x7f080005;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,8 +47,10 @@ public final class R {
         public static final int d_dice=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int last_roll=0x7f050007;
+        public static final int recall=0x7f050009;
         public static final int roll_button=0x7f050004;
         public static final int roll_output=0x7f050006;
+        public static final int save=0x7f050008;
         public static final int title_activity_display_roll=0x7f050005;
     }
     public static final class style {
--- src/com/officerhalf/dxdice/MainActivity.java
+++ src/com/officerhalf/dxdice/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends Activity {
 
 	protected NumberPicker dicePicker;
 	protected NumberPicker sidesPicker;
+	protected int[] lastDice = {0, 0};
+	protected int[] lastRoll;
+	protected int[] savedDice = {0, 0};
+	protected int[] savedRoll = {0};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,7 @@ public class MainActivity extends Activity {
 		dicePicker.setMaxValue(100);
 		dicePicker.setMinValue(1);
 		sidesPicker.setMaxValue(100);
-		sidesPicker.setMinValue(1);
-		
+		sidesPicker.setMinValue(1);		
 	}
 
 	@Override
@@ -42,13 +45,19 @@ public class MainActivity extends Activity {
 		//Get sides, number of dice
 		int dice = dicePicker.getValue();
 		int sides = sidesPicker.getValue();
-		
+
+		lastDice[0] = dice;
+		lastDice[1] = sides;
+
 		int[] rolls = new int[dice];	
 		
 		//Get Rolls
 		Random r = new Random();
 		for(int i = 0; i < dice; i++)
 			rolls[i] = r.nextInt(sides) + 1;
+		
+		//Save roll for recall
+		lastRoll = rolls;
 		
 		//Display Rolls
 		TextView output = (TextView) findViewById(R.id.roll_output);
@@ -68,4 +77,37 @@ public class MainActivity extends Activity {
 		output.setText(outputText);
 	}
 	
+	public void save_roll (View view) {
+
+		//Save the sides/number of dice as well as the totals
+		savedRoll = lastRoll;
+		savedDice[0] = lastDice[0];
+		savedDice[1] = lastDice[1];
+		
+	}
+	
+	public void recall_roll (View view) {
+
+		//Check to see if a roll has been made
+		if(savedRoll[0] != 0)
+		{
+
+			//Show the saved roll
+			TextView output = (TextView) findViewById(R.id.roll_output);
+			String outputText = Integer.toString(savedDice[0]) + "d" + Integer.toString(savedDice[1]) + ":  ";
+			for(int i = 0; i < savedDice[0]; i++)
+			{
+				outputText = outputText + savedRoll[i];
+				if(i != savedDice[0] - 1)
+				{
+					outputText = outputText + "  +  ";
+				}
+			}
+			int sum = 0;
+			for(int i = 0; i < savedDice[0]; i++)
+				sum += savedRoll[i];
+			outputText = outputText + "  =  " + Integer.toString(sum);
+			output.setText(outputText);
+		}
+	}
 }
